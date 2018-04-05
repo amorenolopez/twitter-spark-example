b4910325812ed5e58d03cee89f29618887400354
Arturo <arturofollowill@gmail.com>
2018-04-04 19:32:19 -0500

    Using enviroment variables for secret tokens

--- src/main/scala/com/bigeek/twitter/Main.scala
+++ src/main/scala/com/bigeek/twitter/Main.scala
@@ -1,9 +1,11 @@
 package com.bigeek.twitter
 
+import com.bigeek.twitter.services.TemporaryTables
 import twitter4j.auth.OAuthAuthorization
 import twitter4j.conf.ConfigurationBuilder
 
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.twitter.TwitterUtils
 
@@ -12,22 +14,33 @@ import org.apache.spark.streaming.twitter.TwitterUtils
 object Main {
 
   def main(args: Array[String]): Unit = {
-
     val runLocal = args.length > 0 && args(args.length-1).equals("L")
 
     var conf : SparkConf = new SparkConf()
+    var spark: SparkSession = null
 
-    if (runLocal) {
+      if (runLocal) {
       conf = conf
         .setAppName("twitter-spark-example")
         .setMaster("local[*]")
+       spark = SparkSession
+          .builder()
+          .appName("twitter-spark-example")
+          .master("local[*]")
+          .enableHiveSupport()
+          .getOrCreate()
 
     } else {
       conf = conf
         .setAppName("twitter-spark-example")
-    }
+        spark = SparkSession
+          .builder()
+          .appName("twitter-spark-example")
+          .enableHiveSupport()
+          .getOrCreate()
+      }
 
-    var ssc: StreamingContext = new StreamingContext(conf, Seconds(15))
+    var ssc: StreamingContext = new StreamingContext(spark.sparkContext, Seconds(15))
 
     // Setting access token for twitter API
     val cb = new ConfigurationBuilder
@@ -37,11 +50,15 @@ object Main {
       .setOAuthAccessTokenSecret(System.getenv("TOKEN_SECRET"))
 
     val auth = new OAuthAuthorization(cb.build)
-    val tweets = TwitterUtils.createStream(ssc, Some(auth))
+    val tweet = TwitterUtils.createStream(ssc, Some(auth))
 
+    val temporalyTables = TemporaryTables(spark)
+
+    spark.sqlContext.sql("select * from MAP_TABLE").show()
     // scalastyle:off
-    tweets.print()
+    tweet.print()
     // scalastyle:on
+
 
     ssc.start()
     ssc.awaitTermination()
